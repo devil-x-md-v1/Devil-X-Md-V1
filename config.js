b9d1bcd40a6e97a3e0740e553b4e421ed30a89a0
devil-x-md-v1 <devilxmdofficial@gmail.com>
2026-01-19 23:29:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "2UtG1BQI#figiCdgP2qjIxDHSEY0J-Hs3sFw9rBwzuyHLvl4zp3c",
+SESSION_ID: process.env.SESSION_ID || "iFsGQLoT#wt9CpWwahD09M8uaBsDZ3EN2U5HvN2p-3KofGp2w_No",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://res.cloudinary.com/dhpfn4umb/image/upload/v1768838728/i5awjdyhznmxyi8cm16l.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*HELLO DEVIL X MD ALIVE NOW | POWERD BY SASANKA CHAMUTH*",
-BOT_OWNER: '94774915917',  // Replace with the owner's phone number
+BOT_OWNER: '94784167385',  // Replace with the owner's phone number
 AUTO_STATUS_SEEN: 'true',
 AUTO_STATUS_REACT: 'true',
 
